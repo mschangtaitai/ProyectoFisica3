a8594f56ef733db3d66d5f9cfdffc86d2331cec1
Michel Chan <michelchan@iMac-de-Michel.local>
2019-09-14 21:13:38 -0600

    Base

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Platform : MonoBehaviour
+{
+    public Slider height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(-2, (float)(height.value * 8 + 0.5), -21);
+    }
+}
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Sphere : MonoBehaviour
+{
+
+    public Slider gravity;
+    public Slider height;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        //Physics.gravity = new Vector3(0, -10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown("space")))
+        {
+            this.transform.position = new Vector3(-2, (float)(height.value * 8 + 0.5), -20);
+            this.rb.velocity = new Vector3(0, 0, 0);
+            print(gravity.value);
+            Physics.gravity = new Vector3(0, -gravity.value * 10, 0);
+            rb.AddForce(0, 0, 3, ForceMode.Impulse);
+            print("space was pressed");
+        }
+        
+    }
+}
